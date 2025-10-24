d0d5d3b19c7232cde4ac55c4585152f726643c2c
Gehad Hassan <aubaiwofre@gmail.com>
2025-10-24 22:40:49 +0300

    chore(cache): make TTLCache thread-safe using RLock

--- app/cache.py
+++ app/cache.py
@@ -1,20 +1,25 @@
 import time
 from typing import Any
-
+from threading import RLock
+ 
 class TTLCache:
     def __init__(self, ttl_seconds: int = 60):
         self.ttl = ttl_seconds
         self.store: dict[str, tuple[float, Any]] = {}
+        self._lock = RLock()
 
     def get(self, key: str):
-        val = self.store.get(key)
-        if not val:
-            return None
-        t, data = val
-        if time.time() - t > self.ttl:
-            self.store.pop(key, None)
-            return None
-        return data
+        with self._lock:
+            val = self.store.get(key)
+            if not val:
+                return None
+            t, data = val
+            if time.time() - t > self.ttl:
+                self.store.pop(key, None)
+                return None
+            return data
+ 
 
     def set(self, key: str, value: Any):
-        self.store[key] = (time.time(), value)
+        with self._lock:
+            self.store[key] = (time.time(), value)
